f94c783d1f8143dd0ed4cdf1b79ae617cf2ada64
huangjiaxi <huangjiaxi@oak-inc.com>
2019-08-26 17:57:09 +0800

    pager 左5右5逻辑

--- src/components/Pager/Pager.tsx
+++ src/components/Pager/Pager.tsx
@@ -6,20 +6,48 @@ interface Props {
     pageSize?:number;
     total:number;
     setPageNum:any;
+    pageCount?:number;
 }
 
 const Pager:React.FunctionComponent<Props> = (props) => {
     const pageTotal = Math.ceil( props.total / (props.pageSize as number));
     const createPageBtn = ()=>{
         let listArray = [];
-        for(let i=1;i<=pageTotal;i++) {
+        let pageTexts = generatePageArray();
+        for(let i=0; i < pageTexts.length ; i++) {
             listArray.push(<li
-                className={`pagerItem fl ${ i===props.page ? 'active' : ''}`}
+                className={`pagerItem fl ${ pageTexts[i]===props.page ? 'active' : ''}`}
                 key={i}
                 onClick={()=>props.setPageNum(i)}
-            >{i}</li>)
+            >{pageTexts[i]}</li>)
         }
         return listArray;
+    };
+
+    const generatePageArray = ()=>{
+        return unique([1,pageTotal,
+            props.page,
+            props.page - 1, props.page - 2 ,
+            props.page + 1 ,props.page + 2
+        ]
+            .filter((n)=> n>=1 && n <= pageTotal)
+            .sort((a,b)=> a-b))
+            .reduce((prev:Array<any>,current:number,index:number,array:Array<any>)=>{
+                prev.push(current)
+                array[index + 1] !== undefined && array[index + 1] - array[index] > 1 && prev.push('...')
+                return prev
+            },[])
+    }
+
+    function unique(array:Array<any>) {
+        // array = [1,1,2,3,5,20]
+        // return [... new Set(array)] 兼容性太差
+        const object = {};
+        array.map((num:string) => {
+            // @ts-ignore
+            object[num] = true
+        })
+        return Object.keys(object).map((s)=>parseInt(s))
     }
     const onAddPage = ()=>{
         if(props.page > 1){
@@ -40,7 +68,8 @@ const Pager:React.FunctionComponent<Props> = (props) => {
     )
 };
 Pager.defaultProps = {
-    pageSize:10
+    pageSize:10,
+    pageCount:10
 };
 
 export default Pager;
